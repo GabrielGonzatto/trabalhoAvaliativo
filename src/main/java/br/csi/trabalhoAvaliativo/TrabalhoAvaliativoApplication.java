97b9d652470368a5354867e2b46e1825bbd4fa27
Gabriel <gabegonzatto@gmail.com>
2023-09-20 16:51:57 -0300

    upando o projeto inicial

---
+++ src/main/java/br/csi/trabalhoAvaliativo/TrabalhoAvaliativoApplication.java
@@ -0,0 +1,13 @@
+package br.csi.trabalhoAvaliativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoAvaliativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoAvaliativoApplication.class, args);
+	}
+
+}
